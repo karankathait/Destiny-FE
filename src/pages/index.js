8f5cbb489fe2afb059e13608ae08ff94e36e9545
karankathait <karankathait958@gmail.com>
2024-01-23 14:40:21 +0530

    product pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
+import Navbar from './Component/navbar'
+import Cards from './Component/cards'
+import SingleProduct from './Component/singleProduct'
 
 export default function Home() {
    console.log('here ')
@@ -8,6 +11,9 @@ export default function Home() {
     <main>
        <div className='bg-cyan-100 bg-gradient-to-r h-screen'>
           <h1>hello world, Home Page</h1>
+          <Navbar />
+          <Cards />
+          {/* <SingleProduct /> */}
        </div>
     </main>
   )
