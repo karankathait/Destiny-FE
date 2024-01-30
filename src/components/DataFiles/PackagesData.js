98a25d1f6f5f11f54f30ec5eaafdf6e920e80c8d
karankathait <karankathait958@gmail.com>
2024-01-30 10:06:26 +0530

    gh-pages install

---
+++ src/components/DataFiles/PackagesData.js
@@ -0,0 +1,325 @@
+//International Trips Images
+import InternationalTripsImage1 from "@/images/InternationalTrips/international-1.jpg";
+import InternationalTripsImage2 from "@/images/InternationalTrips/international-2.jpg";
+import InternationalTripsImage3 from "@/images/InternationalTrips/international-3.jpg";
+import InternationalTripsImage4 from "@/images/InternationalTrips/international-4.jpg";
+import InternationalTripsImage5 from "@/images/InternationalTrips/international-5.jpg";
+import InternationalTripsImage6 from "@/images/InternationalTrips/international-6.jpg";
+import InternationalTripsImage7 from "@/images/InternationalTrips/international-7.jpg";
+import InternationalTripsImage8 from "@/images/InternationalTrips/international-8.jpg";
+import InternationalTripsImage9 from "@/images/InternationalTrips/international-9.jpg";
+import InternationalTripsImage10 from "@/images/InternationalTrips/international-10.jpg";
+import InternationalTripsImage11 from "@/images/InternationalTrips/international-11.jpg";
+import InternationalTripsImage12 from "@/images/InternationalTrips/international-12.jpg";
+import InternationalTripsImage13 from "@/images/InternationalTrips/international-13.jpg";
+
+//domestic Trips Images
+import DomesticTripsImage1 from "@/images/DomesticTripsImg/domestic-1.jpg";
+import DomesticTripsImage2 from "@/images/DomesticTripsImg/domestic-2.jpg";
+import DomesticTripsImage3 from "@/images/DomesticTripsImg/domestic-3.jpg";
+import DomesticTripsImage4 from "@/images/DomesticTripsImg/domestic-4.jpg";
+import DomesticTripsImage5 from "@/images/DomesticTripsImg/domestic-5.jpg";
+import DomesticTripsImage6 from "@/images/DomesticTripsImg/domestic-6.jpg";
+import DomesticTripsImage7 from "@/images/DomesticTripsImg/domestic-7.jpg";
+import DomesticTripsImage8 from "@/images/DomesticTripsImg/domestic-8.jpg";
+import DomesticTripsImage9 from "@/images/DomesticTripsImg/domestic-9.jpg";
+
+const PackagesData = {
+  // other packages...
+  internationalPackages: {
+    Singapore: {
+      thumbnail: InternationalTripsImage1,
+      cityName: "Singapore",
+      startingPrice: "80,890",
+      packages: [
+        {
+          id: 1,
+          title:"Best of Singapore",
+          thumbnail: "https://media.easemytrip.com/media/Deal/DL637796638595732983/SightSeeing/SightSeeingWKRy08.jpg",
+          images:[
+            "image1.jpg,",
+            "image2.jpg"
+          ],
+          hotel:true,
+          sightseeing:true,
+          transfer:true,
+          meal:true,
+          fliht:true,
+          price: "37,390",
+          person:2,
+          days: 5,
+          nights: 4,
+          packageOverview: "",
+          Inclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          Exclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          timetable: {
+            day1: "Activity for day 1",
+            day2: "Activity for day 2",
+            // Add more days and activities as needed
+          },
+        },
+        {
+          id: 2,
+          title:"Best of Singapore",
+          thumbnail: "https://media.easemytrip.com/media/Deal/DL637796638595732983/SightSeeing/SightSeeingWKRy08.jpg",
+          images:[
+            "image1.jpg,",
+            "image2.jpg"
+          ],
+          hotel:true,
+          sightseeing:true,
+          transfer:true,
+          meal:true,
+          fliht:true,
+          price: "37,390",
+          person:2,
+          days: 5,
+          nights: 4,
+          packageOverview: "",
+          Inclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          Exclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          timetable: {
+            day1: "Activity for day 1",
+            day2: "Activity for day 2",
+            // Add more days and activities as needed
+          },
+        },
+        {
+          id: 3,
+          title:"Best of Singapore",
+          thumbnail: "https://media.easemytrip.com/media/Deal/DL637796638595732983/SightSeeing/SightSeeingWKRy08.jpg",
+          images:[
+            "image1.jpg,",
+            "image2.jpg"
+          ],
+          hotel:true,
+          sightseeing:true,
+          transfer:true,
+          meal:true,
+          fliht:true,
+          price: "37,390",
+          person:2,
+          days: 5,
+          nights: 4,
+          packageOverview: "",
+          Inclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          Exclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          timetable: {
+            day1: "Activity for day 1",
+            day2: "Activity for day 2",
+            // Add more days and activities as needed
+          },
+        },
+        {
+          id: 4,
+          title:"Best of Singapore",
+          thumbnail: "https://media.easemytrip.com/media/Deal/DL637796638595732983/SightSeeing/SightSeeingWKRy08.jpg",
+          images:[
+            "image1.jpg,",
+            "image2.jpg"
+          ],
+          hotel:true,
+          sightseeing:true,
+          transfer:true,
+          meal:true,
+          fliht:true,
+          price: "37,390",
+          person:2,
+          days: 5,
+          nights: 4,
+          packageOverview: "",
+          Inclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          Exclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          timetable: {
+            day1: "Activity for day 1",
+            day2: "Activity for day 2",
+            // Add more days and activities as needed
+          },
+        },
+        
+        // More packages for Paris
+      ],
+    },
+    Europe: {
+      thumbnail: InternationalTripsImage2,
+      cityName: "Europe",
+      startingPrice: "2,34,600",
+      packages: [
+        {
+          id: 1,
+          title:"Grand Wonders Of Europe",
+          thumbnail: "https://media.easemytrip.com/media/Deal/DL637796638595732983/SightSeeing/SightSeeingWKRy08.jpg",
+          images:[
+            "image1.jpg,",
+            "image2.jpg"
+          ],
+          hotel:true,
+          sightseeing:true,
+          transfer:true,
+          meal:true,
+          fliht:true,
+          price: "37,390",
+          person:2,
+          days: 5,
+          nights: 4,
+          packageOverview: "",
+          Inclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          Exclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          timetable: {
+            day1: "Activity for day 1",
+            day2: "Activity for day 2",
+            // Add more days and activities as needed
+          },
+        },
+        {
+          id: 2,
+          title:"Grand Wonders Of Europe",
+          thumbnail: "https://media.easemytrip.com/media/Deal/DL637796638595732983/SightSeeing/SightSeeingWKRy08.jpg",
+          images:[
+            "image1.jpg,",
+            "image2.jpg"
+          ],
+          hotel:true,
+          sightseeing:true,
+          transfer:true,
+          meal:true,
+          fliht:true,
+          price: "37,390",
+          person:2,
+          days: 5,
+          nights: 4,
+          packageOverview: "",
+          Inclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          Exclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          timetable: {
+            day1: "Activity for day 1",
+            day2: "Activity for day 2",
+            // Add more days and activities as needed
+          },
+        },
+        {
+          id: 3,
+          title:"Grand Wonders Of Europe",
+          thumbnail: "https://media.easemytrip.com/media/Deal/DL637796638595732983/SightSeeing/SightSeeingWKRy08.jpg",
+          images:[
+            "image1.jpg,",
+            "image2.jpg"
+          ],
+          hotel:true,
+          sightseeing:true,
+          transfer:true,
+          meal:true,
+          fliht:true,
+          price: "37,390",
+          person:2,
+          days: 5,
+          nights: 4,
+          packageOverview: "",
+          Inclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          Exclusions:[
+            "1Inclusion",
+            "2Inclusion",
+            "3Inclusion",
+            "4Inclusion",
+          ],
+          timetable: {
+            day1: "Activity for day 1",
+            day2: "Activity for day 2",
+            // Add more days and activities as needed
+          },
+        },
+        {
+          id: 2,
+          image: "tokyo_package_2.jpg",
+          price: "2700",
+          days: 8,
+          nights: 7,
+        },
+        // More packages for Tokyo
+      ],
+    },
+    // More cities with packages
+  },
+  DomesticPackages:{
+    Rishikesh:{
+      thumbnail:"",
+      cityName:"",
+      startingPrice:"",
+      packages:[
+        {
+
+        },
+        {
+          
+        }
+      ]
+    }
+  }
+};
+
+export default PackagesData;
